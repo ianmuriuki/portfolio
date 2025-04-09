7836e0deb466e661cd7cbb59d68465a086b25a30
ianmuriuki <ianmuriuki.inc@gmail.com>
2025-04-09 17:37:44 +0300

    resolve: tailwind css issues, update dependancies

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    images: {
-      domains: ['yourdomain.com'], // if using external images
-    },
-  };
-// This is a Next.js configuration file that specifies the domains from which images can be loaded.
-// This is useful for optimizing images in your application.  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
-import Image from "next/image";
 import ContactForm from "/components/ContactForm";
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
